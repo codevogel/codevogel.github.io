60a1eaaaef7bbf3d2e2c1ebc24fc6ee18b528023
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-27 13:29:16 +0200

    update pls.svx

--- svelte.config.js
+++ svelte.config.js
@@ -9,38 +9,38 @@ import rehypeSlug from 'rehype-slug';
 
 /** @type {import('mdsvex').MdsvexOptions}/ */
 const mdsvexOptions = {
-    extensions: ['.svx'],
-    highlight: {
-        highlighter: async (code, lang = 'text') => {
-            const highlighter = await getHighlighter({
-                themes: ['poimandres'],
-                langs: ['javascript', 'typescript']
-            });
-            await highlighter.loadLanguage('javascript', 'typescript', 'bash', 'yaml');
-            const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
-            return `{@html \`${html}\` }`;
-        }
-    },
-    rehypePlugins: [rehypeSlug, rehypeToc] // in order!
+   extensions: ['.svx'],
+   highlight: {
+      highlighter: async (code, lang = 'text') => {
+         const highlighter = await getHighlighter({
+            themes: ['poimandres'],
+            langs: ['javascript', 'typescript', 'bash', 'yaml'] // include all needed languages here
+         });
+
+         const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
+         return `{@html \`${html}\` }`;
+      }
+   },
+   rehypePlugins: [rehypeSlug, rehypeToc] // in order!
 };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: [preprocess(), vitePreprocess(), mdsvex(mdsvexOptions)],
+   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+   // for more information about preprocessors
+   preprocess: [preprocess(), vitePreprocess(), mdsvex(mdsvexOptions)],
 
-    kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter()
-    },
-    paths: {
-        // Change the first '' to your repository name if you are deploying a project site.
-        base: process.env.NODE_ENV === 'production' ? '' : ''
-    },
-    extensions: ['.svelte', '.svx']
+   kit: {
+      // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+      // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+      // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+      adapter: adapter()
+   },
+   paths: {
+      // Change the first '' to your repository name if you are deploying a project site.
+      base: process.env.NODE_ENV === 'production' ? '' : ''
+   },
+   extensions: ['.svelte', '.svx']
 };
 
 export default config;
