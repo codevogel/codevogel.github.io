4ccf1b5ff4233ac485ca05871c203138544aa715
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-08 14:20:24 +0200

    add main-nav

---
+++ app/src/lib/components/ui/header/navigation/main-nav.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Button from '$lib/components/shadcn-svelte/ui/button/button.svelte';
+	import { pages } from '$lib/routes/routes';
+</script>
+
+<nav class="flex flex-row justify-center">
+	<main class="flex flex-row justify-between max-w-sm">
+		{#each pages as page (page.path)}
+			<Button variant="nav" href={page.path}>{page.name}</Button>
+		{/each}
+	</main>
+</nav>
---
+++ app/src/lib/components/ui/navigation/main-nav.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Button from '$lib/components/shadcn-svelte/ui/button/button.svelte';
+	import { pages } from '$lib/routes/routes';
+</script>
+
+<nav class="flex flex-row">
+	{#each pages as page (page.path)}
+		<Button variant="ghost" href={page.path}>{page.name}</Button>
+	{/each}
+</nav>
