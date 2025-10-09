7c6f91f1f3e90c822a50f9c1b7c4d00b1029ccc5
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-09 14:47:51 +0200

    header formatting

--- app/src/lib/components/ui/header/header.svelte
+++ app/src/lib/components/ui/header/header.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import MainNav from "$lib/components/ui/header/navigation/main-nav.svelte";
-	import LogoCodevogelText from "$lib/components/ui/header/logo/logo-codevogel-text.svelte";
+	import MainNav from '$lib/components/ui/header/navigation/main-nav.svelte';
+	import LogoCodevogelText from '$lib/components/ui/header/logo/logo-codevogel-text.svelte';
 </script>
 
-
-<LogoCodevogelText/>
-<MainNav />
-
+<header class="flex flex-col bg-accent/30 p-4 gap-y-2">
+	<LogoCodevogelText />
+	<MainNav />
+</header>
--- app/src/lib/components/ui/header/logo/logo-codevogel-text.svelte
+++ app/src/lib/components/ui/header/logo/logo-codevogel-text.svelte
@@ -1,1 +1,1 @@
-<img src="/logos/logo-codevogel-text.svg" alt="Codevogel Logo" class="mt-4 mb-2 max-w-sm px-4" />
+<img src="/logos/logo-codevogel-text.svg" alt="Codevogel Logo" class="max-w-sm px-4" />
