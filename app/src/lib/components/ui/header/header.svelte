9abd05e00c7e24074ff75aea5eaa099ea15a67c2
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-09 14:44:45 +0200

    add header

---
+++ app/src/lib/components/ui/header/header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import MainNav from "$lib/components/ui/header/navigation/main-nav.svelte";
+	import LogoCodevogelText from "$lib/components/ui/header/logo/logo-codevogel-text.svelte";
+</script>
+
+
+<LogoCodevogelText/>
+<MainNav />
+
