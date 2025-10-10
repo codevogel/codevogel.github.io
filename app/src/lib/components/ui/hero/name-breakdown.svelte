4b3193a2cb5ffe769b7884a4b324ffcc9b3e5511
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-10 21:50:14 +0200

    add name breakdown

---
+++ app/src/lib/components/ui/hero/name-breakdown.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Volume2 } from 'lucide-svelte';
+
+
+	function playSound() {
+		const audio = new Audio('/sounds/codevogel.mp3');
+		audio.play();
+	}
+</script>
+
+<div class="rounded-xl border-[.1px] border-accent p-4 text-center max-w-sm mx-auto mt-8">
+	<p>
+		<span class="flex flex-row justify-center gap-x-4">
+			<span class="font-serif italic">/ˈkoːdəˌvoːɣəl/</span> <button on:click={playSound}><Volume2 class="stroke-foreground/50 hover:stroke-foreground" /></button></span
+		>
+		A bird that enjoys programming.
+	</p>
+</div>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import BirdHero from '$lib/components/ui/hero/bird-hero.svelte';
+	import NameBreakdown from '$lib/components/ui/hero/name-breakdown.svelte';
 </script>
 
-<div class="mt-12">
-	<BirdHero />
+<div class="px-8">
+		<BirdHero />
+	<NameBreakdown />
 </div>
