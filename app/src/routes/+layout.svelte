4fe12ce42b63f7e8f6986792b1ba4649ebb05363
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-17 15:20:55 +0200

    run prettier

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -12,28 +12,22 @@
 	import { afterNavigate } from '$app/navigation';
 
 	afterNavigate(async () => {
-		await determineSnapBehavior()
-	})
-
+		await determineSnapBehavior();
+	});
 
 	onMount(async () => {
-		await determineSnapBehavior()
+		await determineSnapBehavior();
 	});
 
 	async function determineSnapBehavior() {
-		await new Promise(resolve => setTimeout(resolve, 200));
+		await new Promise((resolve) => setTimeout(resolve, 200));
 
-		const firstSection = document.querySelector('#content-container > :first-child');
+		const firstSection: HTMLElement | null = document.querySelector(
+			'#content-container > :first-child'
+		);
 		const viewportHeight = window.innerHeight;
 		const headerHeight = document.querySelector('header')?.offsetHeight || 0;
 		const availableHeight = viewportHeight - headerHeight;
-
-		console.log(
-			firstSection,
-			firstSection?.offsetHeight > availableHeight,
-			firstSection?.offsetHeight,
-			availableHeight
-		);
 
 		if (firstSection && firstSection.offsetHeight > availableHeight) {
 			firstSection.classList.add('overflowing');
@@ -46,18 +40,6 @@
 </svelte:head>
 
 <Toaster />
-
-<!-- <div class="max-h-[100dvh] snap-y snap-mandatory overflow-y-scroll"> -->
-<!-- 	<header class="h-header snap-none bg-red-300"></header> -->
-<!-- 	<main -->
-<!-- 		class="[&>*]:min-h-page [&>*]:snap-start [&>:first-child]:snap-end [&>:first-child]:min-h-page-without-header [&>:last-child]:min-h-page-without-footer" -->
-<!-- 	> -->
-<!-- 		<section class="bg-red-500"></section> -->
-<!-- 		<section class="bg-red-600"></section> -->
-<!-- 		<section class="bg-red-700"></section> -->
-<!-- 	</main> -->
-<!-- 	<footer class="h-footer snap-end bg-red-400"></footer> -->
-<!-- </div> -->
 
 <div id="scroll-container" class="h-[100dvh] max-h-[100dvh] snap-y snap-mandatory overflow-y-auto">
 	<header id="site-header" class="h-header snap-start"><Header /></header>
