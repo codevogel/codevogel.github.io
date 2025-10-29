4244cbd7504c171eb96fa9a1fb5dd956c3315d31
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-29 12:43:34 +0100

    feat: create script to fetch and save fallback README files for projects

---
+++ app/scripts/fetch-fallback-readmes.ts
@@ -0,0 +1,33 @@
+import { projects } from '../src/lib/server/data/projects.ts';
+import { existsSync, writeFileSync } from 'fs';
+
+async function main() {
+	await fetchFallbackReadmes();
+}
+
+async function fetchFallbackReadmes() {
+	const projectsToFetch = projects
+		.filter((p) => p.readmeURL !== undefined)
+		.map((p) => ({ slug: p.slug, url: p.readmeURL, type: p.type }));
+
+	for (const project of projectsToFetch) {
+		const res = await fetch(project.url!);
+		if (!res.ok) {
+			console.error(
+				`Failed to fetch README for ${project.slug} (${project.type}): ${res.status} ${res.statusText}`
+			);
+			continue;
+		}
+		const readmeContent = await res.text();
+		const dirPath = `./static/projects/${project.type}s/${project.slug}`;
+
+		if (!existsSync(dirPath)) {
+			throw new Error(`Directory does not exist: ${dirPath}`);
+		}
+
+		const filePath = `${dirPath}/README.md`;
+		writeFileSync(filePath, readmeContent, 'utf8');
+	}
+}
+
+await main();
