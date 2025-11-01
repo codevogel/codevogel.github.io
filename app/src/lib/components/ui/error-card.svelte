0ae4e98bf07dbceb2c3bcae8512797ba6a0b6c4f
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-01 12:04:50 +0100

    feat(ui): add error card component and error page route

---
+++ app/src/lib/components/ui/error-card.svelte
@@ -0,0 +1,25 @@
+<script>
+	import * as Card from '$lib/components/shadcn-svelte/ui/card';
+
+	import { page } from '$app/state';
+
+	const status = $derived(page.status);
+	const errorMessage = $derived(page.error?.message ?? undefined);
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<h1>Oops! 😦</h1>
+		<p class="subscript">Something went wrong while trying to load this page.</p>
+	</Card.Header>
+	<Card.Content>
+		<h2>
+			{status}
+		</h2>
+		{#if errorMessage}
+			<p>
+				{errorMessage}
+			</p>
+		{/if}
+	</Card.Content>
+</Card.Root>
---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ErrorCard from '$lib/components/ui/error-card.svelte';
+	import Section from '$lib/components/ui/section.svelte';
+</script>
+
+<Section class="justify-center px-8">
+	<ErrorCard />
+</Section>
