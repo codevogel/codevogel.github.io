314c418d93b150e1f1b199b18cce7554b8b22d5b
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-02 19:31:59 +0100

    fix(ui): add Section to contact page

--- app/src/routes/contact/+page.svelte
+++ app/src/routes/contact/+page.svelte
@@ -1,43 +1,46 @@
 <script>
 	import FindMeOn from '$lib/components/ui/find-me-on.svelte';
+	import Section from '$lib/components/ui/section.svelte';
 	import { CopyIcon, FileUserIcon } from 'lucide-svelte';
 	import { toast } from 'svelte-sonner';
 </script>
 
-<div class="prose text-center dark:prose-invert prose-headings:my-2 prose-p:my-2">
-	<h1>Contact</h1>
-	<p>I am currently <span class="text-primary">open to work</span>.</p>
-	<p>
-		<span class="font-bold text-primary italic">Are you interested in working together?</span><br
-		/>Then, I would love to hear from you!<br />Please don't hesitate to contact me at:
-	</p>
-	<button
-		class="text-md mx-auto flex flex-row items-center justify-between gap-x-4 rounded-2xl bg-primary px-4 py-3 font-mono font-extrabold text-background hover:bg-foreground hover:text-background"
-		onclick={() => {
-			navigator.clipboard.writeText('contact@codevogel.com');
-			toast(
-				"✅ My e-mail address has been copied to your clipboard. I'm excited to hear from you!"
-			);
-		}}
-	>
-		contact<span class="hidden">-dont-spam-me</span>@codevogel.com
-		<CopyIcon size={16} />
-	</button>
-	<p>
-		Feel free to contact me for anything else as well.
-		<br />Looking forward to your message!
-	</p>
-	<p>
-		<span class="font-bold text-primary italic">Are you a recruiter?</span><br />Then swipe down
-		to see my resume 😉
-	</p>
-</div>
+<Section>
+	<div class="prose text-center dark:prose-invert prose-headings:my-2 prose-p:my-2">
+		<h1>Contact</h1>
+		<p>I am currently <span class="text-primary">open to work</span>.</p>
+		<p>
+			<span class="font-bold text-primary italic">Are you interested in working together?</span
+			><br />Then, I would love to hear from you!<br />Please don't hesitate to contact me at:
+		</p>
+		<button
+			class="text-md mx-auto flex flex-row items-center justify-between gap-x-4 rounded-2xl bg-primary px-4 py-3 font-mono font-extrabold text-background hover:bg-foreground hover:text-background"
+			onclick={() => {
+				navigator.clipboard.writeText('contact@codevogel.com');
+				toast(
+					"✅ My e-mail address has been copied to your clipboard. I'm excited to hear from you!"
+				);
+			}}
+		>
+			contact<span class="hidden">-dont-spam-me</span>@codevogel.com
+			<CopyIcon size={16} />
+		</button>
+		<p>
+			Feel free to contact me for anything else as well.
+			<br />Looking forward to your message!
+		</p>
+		<p>
+			<span class="font-bold text-primary italic">Are you a recruiter?</span><br />Then swipe
+			down to see my resume 😉
+		</p>
+	</div>
+</Section>
 
-<div class="flex flex-col items-center justify-center gap-y-8">
+<Section class="items-center justify-center gap-y-8">
 	<FindMeOn />
 	<h1>My Resume</h1>
 	<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
 	<a href="/pdf/resume-kamiel-de-visser.pdf">
 		<FileUserIcon size={48} class="hover:stroke-primary" />
 	</a>
-</div>
+</Section>
