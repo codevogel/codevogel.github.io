fe6d63eea5b08b176de5f17bb3de79b48d299964
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-03 23:05:31 +0100

    feat(svelte): add prerender error handling in config

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,12 +1,38 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const toolsPathExpr = /\/tools\/.*/;
+const gamesPathExpr = /\/games\/.*/;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				if (toolsPathExpr.test(path) || gamesPathExpr.test(path)) {
+					console.warn(`Pre-rendering error for ${path} (referrer: ${referrer}): ${message}`);
+					return;
+				}
+				throw new Error(`Pre-rendering failed for ${path} (referrer: ${referrer}): ${message}`);
+			},
+			handleMissingId: ({ path, referrers, message }) => {
+				if (toolsPathExpr.test(path) || gamesPathExpr.test(path)) {
+					console.warn(
+						`Missing ID during pre-rendering for ${path} (referrers: ${referrers.join(', ')}): ${message}`
+					);
+					return;
+				}
+				throw new Error(
+					`Missing ID during pre-rendering for ${path} (referrers: ${referrers.join(', ')}): ${message}`
+				);
+			}
+		},
+
+		adapter: adapter()
+	}
 };
 
 export default config;
